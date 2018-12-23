13e9c7bbcb87029dac10362816f26a1ce45322ff
andrea <andrea@andreas-MacBook-Pro.local>
2018-12-23 20:55:10 +0100

    added colors palette

---
+++ PokeApp/PokeApp/Resources/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  PokeApp
+//
+//  Created by andrea on 23/12/2018.
+//  Copyright © 2018 andrea. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    static let black = UIColor(named: "Black") ?? UIColor.red
+    static let granet = UIColor(named: "Garnet") ?? UIColor.red
+    static let green = UIColor(named: "Green") ?? UIColor.red
+    static let purple = UIColor(named: "Purple") ?? UIColor.red
+    static let white = UIColor(named: "White") ?? UIColor.red
+}
